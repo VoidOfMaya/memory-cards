af8cfcafda300d15e7684c28591c94e3191f0509
VoidOfMaya <mayaofthevoid@gmail.com>
2025-12-03 12:47:19 +0200

    feat(card):trigger a card select event

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import '../styles/card.css'
 ;
-function Card({photo}){
+function Card({photo, hasbeenCLicked = false}){
+    const [status, setStatus] = useState(false)
+    //color change for development purposes
+    const [bgColor, setBgColor] = useState("white");
 
     return(
-    <div className="card">
+    <div className="card"
+        onClick={()=>{
+            if(!hasbeenCLicked){
+                setStatus(true);
+                setBgColor(bgColor === "white" ? "lightblue" : "white");
+                console.log(status)
+                
+            }
+         }}
+        style={{backgroundColor: bgColor}}
+        >
         <img src={photo} alt="randomly generated photo"
                          height='300px'
                          width='300px'></img>
-        <h3>card example</h3>
+        <h3>card example </h3>
 
     </div>
 )
